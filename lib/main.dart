9f790ec6390631d9b8b1b771290b2c92eebf0684
123.vipulj@gmail.com <viPUl123jurel>
2021-05-17 17:35:11 +0530

    padding remove fromt corner

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:flutter/material.dart';
 import 'bloc/bloc_provider.dart';
 import 'bloc/fetch_data.dart';
 
-void main() async{
+void main() async {
   runApp(MyApp());
 }
 
@@ -33,26 +33,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
- Widget _getData() {
+Widget _getData() {
   return FutureBuilder(
-      future:  DBProvider.db.getCatCount(),
+      future: DBProvider.db.getCatCount(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (!snapshot.hasData) {
           return Center(child: Text("Wait ..."));
         } else {
-          if(snapshot != null){
-            if(snapshot.data > 1){
+          if (snapshot != null) {
+            if (snapshot.data > 1) {
               return HomePage();
             } else {
+              // ignore: missing_return
               return _blockData();
             }
           }
         }
-      }
-      );
-
-
+      });
 }
 
 Widget _blockData() {
@@ -62,10 +59,7 @@ Widget _blockData() {
     bloc: bloc,
     child: Container(
       height: 280,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: _buildStreamBuilder(bloc),
-      ),
+      child: _buildStreamBuilder(bloc),
     ),
   );
 }
@@ -74,7 +68,7 @@ Widget _buildStreamBuilder(FetchData result) {
   return StreamBuilder(
     stream: result.actorStream,
     // ignore: missing_return
-    builder: (context, snapshot){
+    builder: (context, snapshot) {
       final results = snapshot.data;
       if (results == null) {
         return Center(
@@ -84,50 +78,64 @@ Widget _buildStreamBuilder(FetchData result) {
 
       EProduct eProduct = results.body;
       //Categories
-      for (int i=0;i< eProduct.categories.length; i++){
-        Categories categories = Categories(id: eProduct.categories[i].id, name: eProduct.categories[i].name);
+      for (int i = 0; i < eProduct.categories.length; i++) {
+        Categories categories = Categories(
+            id: eProduct.categories[i].id, name: eProduct.categories[i].name);
         DBProvider.db.createEmployee(categories);
         // Products
-        for(int j=0;j<eProduct.categories[i].products.length;j++){
-          Products products = Products(id : eProduct.categories[i].products[j].id,
-              name: eProduct.categories[i].products[j].name, cat_Id: eProduct.categories[i].id);
-              DBProvider.db.createProduct(products);
-              //variants
-              for(int k=0;k < eProduct.categories[i].products[j].variants.length;k++){
-                Variants variants = Variants(color: eProduct.categories[i].products[j].variants[k].color,
-                    size: eProduct.categories[i].products[j].variants[k].size,
-                price : eProduct.categories[i].products[j].variants[k].price,
+        for (int j = 0; j < eProduct.categories[i].products.length; j++) {
+          Products products = Products(
+              id: eProduct.categories[i].products[j].id,
+              name: eProduct.categories[i].products[j].name,
+              cat_Id: eProduct.categories[i].id);
+          DBProvider.db.createProduct(products);
+          //variants
+          for (int k = 0;
+              k < eProduct.categories[i].products[j].variants.length;
+              k++) {
+            Variants variants = Variants(
+                color: eProduct.categories[i].products[j].variants[k].color,
+                size: eProduct.categories[i].products[j].variants[k].size,
+                price: eProduct.categories[i].products[j].variants[k].price,
                 product_id: eProduct.categories[i].products[j].id);
-                DBProvider.db.createVariants(variants);
-              }
+            DBProvider.db.createVariants(variants);
+          }
 
-              //tax
-              Tax tax = Tax(name: eProduct.categories[i].products[j].tax.name,
-                  value: eProduct.categories[i].products[j].tax.value,
-                  product_id: eProduct.categories[i].products[j].id);
-              DBProvider.db.createTax(tax);
+          //tax
+          Tax tax = Tax(
+              name: eProduct.categories[i].products[j].tax.name,
+              value: eProduct.categories[i].products[j].tax.value,
+              product_id: eProduct.categories[i].products[j].id);
+          DBProvider.db.createTax(tax);
         }
 
-        for(int m=0;m < eProduct.categories[i].child_categories.length; m++){
-          ChildCat childCat = ChildCat(child_cat: eProduct.categories[i].child_categories[m],
+        for (int m = 0;
+            m < eProduct.categories[i].child_categories.length;
+            m++) {
+          ChildCat childCat = ChildCat(
+              child_cat: eProduct.categories[i].child_categories[m],
               cat_id: eProduct.categories[i].id);
           DBProvider.db.createChildCat(childCat);
         }
-
       }
 
       //Rankings
-      for(int n=0; n<eProduct.rankings.length; n++){
-
-        for(int p=0; p < eProduct.rankings[n].products.length; p++){
-          if(n == 0){
-            ViewCount viewCount = ViewCount(id : eProduct.rankings[n].products[p].id , view_c: eProduct.rankings[n].products[p].viewCount);
+      for (int n = 0; n < eProduct.rankings.length; n++) {
+        for (int p = 0; p < eProduct.rankings[n].products.length; p++) {
+          if (n == 0) {
+            ViewCount viewCount = ViewCount(
+                id: eProduct.rankings[n].products[p].id,
+                view_c: eProduct.rankings[n].products[p].viewCount);
             DBProvider.db.createViewCount(viewCount);
-          } else if(n == 1){
-            OrderCount orderCount = OrderCount(id : eProduct.rankings[n].products[p].id , order_c: eProduct.rankings[n].products[p].orderCount);
+          } else if (n == 1) {
+            OrderCount orderCount = OrderCount(
+                id: eProduct.rankings[n].products[p].id,
+                order_c: eProduct.rankings[n].products[p].orderCount);
             DBProvider.db.createOrderCount(orderCount);
-          } else if(n == 2){
-            ShareCount shareCount = ShareCount(id : eProduct.rankings[n].products[p].id , share_c: eProduct.rankings[n].products[p].mostShares);
+          } else if (n == 2) {
+            ShareCount shareCount = ShareCount(
+                id: eProduct.rankings[n].products[p].id,
+                share_c: eProduct.rankings[n].products[p].mostShares);
             DBProvider.db.createShareCount(shareCount);
           }
         }
